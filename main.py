5f2e71ed8b2e317262707af60e6051ee9e04b932
Mark Vavilov <vavilovmark@gmail.com>
2023-05-05 13:19:19 +0400

    fix

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pickle
 from itertools import repeat
 from multiprocessing import Pool
 from statistics import variance, mean
+from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -31,7 +32,7 @@ class TestResult:
         return f'TestResult: ({self.sample_name} ' \
                f'{self.theory} {self.solver} {self.assert_time} {self.check_time} {self.time} {self.status})'
 
-    def all_unknown(self):
+    def all_unknown(self) -> bool:
         return np.all(self.status == "UNKNOWN")
 
     def array_without_unknowns(self, array: np.ndarray):
@@ -41,7 +42,8 @@ class TestResult:
         # mean of not unknown status
         if self.all_unknown():
             return None
-        return mean(self.array_without_unknowns(self.check_time))
+        a = self.array_without_unknowns(self.time)
+        return mean(a)
 
     def get_max_time(self):
         if self.all_unknown():
@@ -76,9 +78,10 @@ def f(solver, data_dir, show_z3):
     # plt.plot(x, d, label=solver)
     return x, d
 
+
 def show_graph(data_dir='data/np-comp-check', show_z3=False, log=True):
-    with Pool() as pool:
-        xd = pool.starmap(f, zip(solvers, repeat(data_dir), repeat(show_z3)))
+    with Pool() as p:
+        xd = p.starmap(f, zip(solvers, repeat(data_dir), repeat(show_z3)))
         for (x, d), solver in zip(xd, solvers):
             if x is not None:
                 plt.plot(x, d, label=solver)
@@ -94,14 +97,22 @@ def show_graph(data_dir='data/np-comp-check', show_z3=False, log=True):
     plt.show()
 
 
+DICTS_DIR = 'data/dicts'
+
+
 def load_dict(solver: str) -> dict[str, TestResult]:
-    with open(f'data/dicts/{solver}.pkl', 'rb') as f:
-        return pickle.load(f)
+    with open(f'{DICTS_DIR}/{solver}.pkl', 'rb') as file:
+        return pickle.load(file)
+
+
+def string_np_array_to_int(a: np.ndarray):
+    return np.array([int(x) for x in a])
 
 
 def create_and_save_dict(solver: str):
     file = f'data/{solver}.csv'
     df = pd.read_csv(file, index_col=False)
+    print(f'create dict {solver}')
 
     #     create dict from non-unique test names to list of TestResults
     def function(x):
@@ -118,23 +129,23 @@ def create_and_save_dict(solver: str):
     res = df.groupby('sample name').apply(function).to_dict()
 
     # mkdirs
-    os.makedirs('data/dicts', exist_ok=True)
-    with open(f'data/dicts/{solver}.pkl', 'wb') as f:
-        pickle.dump(res, f)
+    os.makedirs(DICTS_DIR, exist_ok=True)
+    with open(f'{DICTS_DIR}/{solver}.pkl', 'wb') as out_file:
+        pickle.dump(res, out_file)
     return res
 
 
-def save_comp_to_z3(solver, z3_dict, out_dir='data/np-comp-check'):
-    print(f'Computing {solver}')
+def save_comp_to_z3(solver: str, z3_dict_to_comp: dict[str, TestResult], out_dir: str = 'data/np-comp-check'):
     if solver == 'Z3':
         return
-    compare_to_z3 = []  # solver -> np array of relative times
-    d = load_dict(solver)
+    print(f'Computing {solver}')
+    compare_to_z3: list[Any] = []  # solver -> np array of relative times
+    d: dict[str, TestResult] = load_dict(solver)
     print(f'Loaded {solver}')
     for sample, results in d.items():
-        if sample not in z3_dict:
+        if sample not in z3_dict_to_comp:
             continue
-        z3_results = z3_dict[sample]
+        z3_results = z3_dict_to_comp[sample]
         if results.all_unknown() or z3_results.all_unknown():
             continue
         a = results.get_average_time()
@@ -144,24 +155,24 @@ def save_comp_to_z3(solver, z3_dict, out_dir='data/np-comp-check'):
     print(f'Saved {solver}')
 
 
-def save_np_array(solver, out_dir, f=lambda x: x.get_average_time()):
+def save_np_array(solver, out_dir, method=lambda x: x.get_average_time()):
     print(f'Computing {solver}')
     compare_to_z3 = []  # solver -> np array
     d = load_dict(solver)
     print(f'Loaded {solver}')
     for sample, results in d.items():
-        a = f(results)
+        a = method(results)
         if a is not None:
             compare_to_z3.append(a)
     np.save(f'{out_dir}/{solver}.npy', np.sort(np.asarray(compare_to_z3)))
     print(f'Saved {solver}')
 
 
-def print_percentes(dir, show_z3=False):
+def print_percentes(input_dir, show_z3=False):
     for solver in solvers:
         if solver == "Z3" and not show_z3:
             continue
-        d = np.load(f'{dir}/{solver}.npy')
+        d = np.load(f'{input_dir}/{solver}.npy')
 
         print(f'{solver} size: {d.size}')
         percentiles = [0, 5, 15, 20, 25, 50, 90, 95, 100]
@@ -174,32 +185,37 @@ if __name__ == '__main__':
     files = [f'data/{solver}.csv' for solver in solvers]
     header = ['sample name', 'theory', 'solver', 'assert time', 'check time', 'time', 'status']
 
-    # to add header and change sep in all csv files
     # for solver in solvers:
-    # solver = 'Bitwuzla'
-    # f = f'data/{solver}.csv'
-    # print(f)
-    # df = pd.read_csv(f, header=None, sep=' \| ', index_col=False, skipinitialspace=True)
-    # df.to_csv(f'data/{solver}.csv', index=False, header=header)
+    #     f = f'data/{solver}.csv'
+    #     print(f)
+    #     df = pd.read_csv(f, index_col=False)
+    #     df.loc[df["sample name"] != 'sample name'].to_csv(f'data/{solver}.csv', index=False, header=header)
+
 
     # to create dict for each solver
     # with Pool() as pool:
     #     result = pool.map(create_and_save_dict, solvers)
 
-    print('Creating Z3 dict')
+    # print('Creating Z3 dict')
+    #
 
     z3_dict = load_dict('Z3')
     path = 'data/np-comp-check'
     os.makedirs(path, exist_ok=True)
-    print('start save to np')
-
+    # print('start save to np')
     with Pool() as pool:
         pool.starmap(save_comp_to_z3, zip(solvers, repeat(z3_dict), repeat(path)))
 
-    print('start show graph')
+    # print('start show graph')
     show_graph(path, show_z3=False, log=True)
 
-    print('start print percentiles')
-    print_percentes(path, show_z3=False)
+    # print('start print percentiles')
+    # print_percentes(path, show_z3=False)
+
+    # print first from z3_dict
+    # for sample, results in z3_dict.items():
+    #     print(sample)
+    #     print(results)
+    #     break
 
     pass
